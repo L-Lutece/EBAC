1da8d000f0400295b6edb86c6c96f2f1cc6acaac
Lutece <datsik771@outlook.com>
2022-12-19 13:58:23 -0300

    tosca

---
+++ modulo 025 - JavaScript/ww2.js
@@ -0,0 +1,37 @@
+let tanks = [
+    {
+        name: 'IS-1',
+        type: 'Heavy-tank',
+        nation: 'Soviet Union',
+        year: '1943'
+    },
+
+    {
+        name: 'Panzerkampfwagen VI Tiger Ausf.E (Tiger I)',
+        type: 'Heavy-tank',
+        nation: 'Germany',
+        year: '1942'
+    },
+
+    {
+        name: 'L6/40',
+        type: 'Ligth-tank',
+        nation: 'Italy',
+        year: '1943'
+    },
+
+    {
+        name: 'Strv m/42',
+        type: 'Medium-tank',
+        nation: 'Sweden',
+        year: '1943'
+    },
+
+    {
+        name: 'ISU-152s',
+        type: 'Anti-tank',
+        nation: 'Soviet-union/China',
+        year: '1944'
+    }
+
+]
